d5b1300b85db8c9c9abe37472133fb1b01dcb361
zhouzhezz <zorichzorich@gmail.com>
2026-05-07 22:11:34 -0400

    Reuse fresh forall transport in soundness helpers

--- ChoiceTyping/theories/SoundnessHelpers.v
+++ ChoiceTyping/theories/SoundnessHelpers.v
@@ -344,21 +344,8 @@ Lemma res_models_fresh_forall_transport
   m ⊨ fresh_forall D body2.
 Proof.
   intros Hscope Hbody Hm.
-  unfold res_models, res_models_with_store in *.
-  simpl in *.
-  destruct Hm as [_ [L [HL Hforall]]].
-  split; [exact Hscope |].
-  exists L. split; [exact HL |].
-  intros y Hy m' Hdom Hrestr.
-  specialize (Hforall y Hy m' Hdom Hrestr).
-  assert (Hsrc : m' ⊨ formula_rename_atom (fresh_for D) y (body1 (fresh_for D))).
-  {
-    eapply res_models_with_store_fuel_irrel; [| | exact Hforall];
-      rewrite formula_rename_preserves_measure; simpl; lia.
-  }
-  pose proof (Hbody y m' Hsrc) as Htarget.
-  eapply res_models_with_store_fuel_irrel; [| | exact Htarget];
-    rewrite formula_rename_preserves_measure; simpl; lia.
+  unfold res_models in *.
+  eapply res_models_with_store_fresh_forall_transport; eauto.
 Qed.
 
 Lemma res_models_fresh_forall_transport2
@@ -371,21 +358,8 @@ Lemma res_models_fresh_forall_transport2
   m ⊨ fresh_forall D2 body2.
 Proof.
   intros Hscope Hbody Hm.
-  unfold res_models, res_models_with_store in *.
-  simpl in *.
-  destruct Hm as [_ [L [HL Hforall]]].
-  split; [exact Hscope |].
-  exists L. split; [exact HL |].
-  intros y Hy m' Hdom Hrestr.
-  specialize (Hforall y Hy m' Hdom Hrestr).
-  assert (Hsrc : m' ⊨ formula_rename_atom (fresh_for D1) y (body1 (fresh_for D1))).
-  {
-    eapply res_models_with_store_fuel_irrel; [| | exact Hforall];
-      rewrite formula_rename_preserves_measure; simpl; lia.
-  }
-  pose proof (Hbody y m' Hsrc) as Htarget.
-  eapply res_models_with_store_fuel_irrel; [| | exact Htarget];
-    rewrite formula_rename_preserves_measure; simpl; lia.
+  unfold res_models in *.
+  eapply res_models_with_store_fresh_forall_transport; eauto.
 Qed.
 
 Lemma res_models_exists_intro (m : WfWorld) (x : atom) (φ : FormulaQ) :
