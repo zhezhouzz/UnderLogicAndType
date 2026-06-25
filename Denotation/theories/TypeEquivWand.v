157373d4c8f6b7f7e70d2b30489890bc60f12bbd
zhouzhezz <zorichzorich@gmail.com>
2026-06-25 15:09:56 -0400

    Simplify wand product regular facts

--- Denotation/theories/TypeEquivWand.v
+++ Denotation/theories/TypeEquivWand.v
@@ -1042,51 +1042,10 @@ Proof.
 	      pose proof (raw_le_dom (m : WorldT)
 	        (res_product n m Hc : WorldT)
 	        (res_product_le_r n m Hc)) as Hdom_r.
-	      assert (Hy_prod : y ∈ world_dom (res_product n m Hc : WorldT)).
-	      {
-	        apply Hdom_l.
-	        pose proof (ty_denote_gas_guard gas
-	          (<[LVFree y := erase_ty τx]>
-	            (relevant_env Σ (CTWand τx τr) e2))
-	          τx (tret (vfvar y)) n Harg) as Hguard_arg_for_y.
-	        repeat rewrite res_models_and_iff in Hguard_arg_for_y.
-	        destruct Hguard_arg_for_y as [_ [Hworld_arg_for_y _]].
-	        assert (Hworld_y :
-	            n ⊨ basic_world_formula
-	              ((<[LVFree y := erase_ty τx]>
-	                (∅ : gmap logic_var ty)) : lty_env)).
-	        {
-		          eapply (basic_world_formula_subenv
-		            ((<[LVFree y := erase_ty τx]>
-		              (∅ : gmap logic_var ty)) : lty_env)
-		            (relevant_env
-		              (<[LVFree y := erase_ty τx]>
-		                (relevant_env Σ (CTWand τx τr) e2))
-		              τx (tret (vfvar y))) n).
-		          - intros v T Hlook.
-		            rewrite lookup_insert_Some in Hlook.
-		            destruct Hlook as [[<- <-]|[Hne Hlook]].
-		            + unfold relevant_env, lty_env_restrict_lvars.
-		              change ((storeA_restrict
-		                (<[LVFree y := erase_ty τx]>
-		                  (relevant_env Σ (CTWand τx τr) e2))
-		                (relevant_lvars τx (tret (vfvar y))) : lty_env) !!
-		                LVFree y = Some (erase_ty τx)).
-		              apply storeA_restrict_lookup_some_2.
-		              * rewrite lookup_insert_eq. reflexivity.
-			              * unfold relevant_lvars.
-		                cbn [tm_lvars tm_lvars_at value_lvars_at lvar_value_keys].
-		                set_solver.
-		            + rewrite lookup_empty in Hlook. congruence.
-		          - exact Hworld_arg_for_y.
-	        }
-		        apply basic_world_formula_models_iff in Hworld_y
-		          as [_ [Hdom_y _]].
-		        rewrite dom_insert_L, dom_empty_L in Hdom_y.
-		        rewrite lvars_fv_union, lvars_fv_empty,
-		          lvars_fv_singleton_free in Hdom_y.
-		        apply Hdom_y.
-		        apply elem_of_union_l. apply elem_of_singleton. reflexivity.
+		      assert (Hy_prod : y ∈ world_dom (res_product n m Hc : WorldT)).
+		      {
+		        apply Hdom_l.
+		        eapply ty_denote_gas_ret_fvar_world_dom. exact Harg.
 		      }
 		      intros a Ha.
 		      apply elem_of_union in Ha as [Ha|Ha].
